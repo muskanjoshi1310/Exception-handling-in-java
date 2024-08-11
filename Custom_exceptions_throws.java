60ad4dbe0ea1bde9ed1daacaa75d408f58e1e428
Muskan joshi <muskanjoshi1310@gmail.com>
2024-08-11 16:45:46 +0530

    custom throw exception
    hello guys, this code points the  link between user and predefined code .Java, Exception Handling is one of the effective means to handle runtime errors so that the regular flow of the application can be preserved. Java Exception Handling is a mechanism to handle runtime errors such as ClassNotFoundException, IOException, SQLException, RemoteException, etc.

--- Custom_exceptions_throws.java
+++ Custom_exceptions_throws.java
@@ -1,15 +1,13 @@
-
-// throw keyword helps to throw link between user defined  and inbuild exception classes
 import java.util.Scanner;
 class ownException{
     public static void main(String[]args){
-        Scanner sc= new Scanner(System.in);// scanner
+        Scanner sc= new Scanner(System.in);
         System.out.println("enter the age");
 try{
         int age= sc.nextInt();
         if(age>100){
             System.out.println("this is throwns");
-            throw new createException();// throw createException from Exception
+            throw new createException();
             
         }
     }catch(Exception e){
@@ -19,6 +17,7 @@ try{
     }
 }
 class createException extends Exception{
-// extends user defined to inbuild exception class
+
+
 }
 
